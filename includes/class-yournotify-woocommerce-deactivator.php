7ca746a136bd20fc4cb94c892ea6f10a91aff03f
Douglas Kendyson <kendyson@kendyson.com>
2023-10-07 13:53:31 +0100

    Initial commit

---
+++ includes/class-yournotify-woocommerce-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://kendyson.com
+ * @since      1.0.0
+ *
+ * @package    Yournotify_Woocommerce
+ * @subpackage Yournotify_Woocommerce/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Yournotify_Woocommerce
+ * @subpackage Yournotify_Woocommerce/includes
+ * @author     Douglas Kendyson <kendyson@kendyson.com>
+ */
+class Yournotify_Woocommerce_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
